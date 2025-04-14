cdf9370e31e3bd10241f391877aa711796f7906f
Oskar Lundh <oskar@startkoden.com>
2025-04-14 17:34:59 +0200

    Fix schema

--- lib/phoenix_image_tools/schema.ex
+++ lib/phoenix_image_tools/schema.ex
@@ -31,43 +31,6 @@ defmodule PhoenixImageTools.Schema do
   defmacro __using__(_opts) do
     quote do
       use Waffle.Ecto.Schema
-
-      import Ecto.Changeset
-
-      @doc """
-      A changeset helper that handles attachment fields and supports the "delete" virtual field
-      pattern for removing existing uploads.
-      """
-      def cast_attachments(changeset, attrs, fields, opts \\ []) do
-        changeset = Waffle.Ecto.Schema.cast_attachments(changeset, attrs, fields, opts)
-
-        Enum.reduce(fields, changeset, fn field, acc ->
-          delete_field = String.to_existing_atom("#{field}_delete")
-
-          case get_change(acc, field) do
-            nil ->
-              if get_field(acc, delete_field) do
-                put_change(acc, field, nil)
-              else
-                acc
-              end
-
-            _file ->
-              acc
-          end
-        end)
-      end
-
-      @doc """
-      Helper to mark an attachment for deletion based on a virtual "delete" field.
-      """
-      def maybe_mark_for_deletion(changeset) do
-        if get_change(changeset, :delete) do
-          %{changeset | action: :delete}
-        else
-          changeset
-        end
-      end
     end
   end
 end
--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule PhoenixImageTools.MixProject do
   use Mix.Project
 
-  @version "0.1.0"
+  @version "0.2.0"
   @source_url "https://github.com/rakso-startkoden/phoenix_image_tools"
 
   def project do
