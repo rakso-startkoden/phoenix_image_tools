b62acf07e8736e910c9a01c7b13296620905995b
Oskar Lundh <oskar@startkoden.com>
2025-08-15 10:08:14 +0200

    Bump version

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule PhoenixImageTools.MixProject do
   use Mix.Project
 
-  @version "0.3.0"
+  @version "0.3.1"
   @source_url "https://github.com/rakso-startkoden/phoenix_image_tools"
 
   def project do
